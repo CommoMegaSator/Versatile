d6bc5b67ad650e9875e602a34a7b4ce2f45bb7fe
CommoMegaSator <valentyn.vivcharyk@gmail.com>
2022-02-19 17:41:55 +0200

    rename from versatile-shop to versatile

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Technologies that you need:
 
 Kafka container not working properly yet.
 Download and start it locally using:
-bin/kafka-server-start.sh config/server.properties <br>
+bin/kafka-server-start.sh config/server.properties 
+<br>
 bin/zookeeper-server-start.sh config/zookeeper.properties
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>stop this
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p>
+        <button onClick={function (){
+          axios.get('backend:5000').then(
+              function (response){
+                alert(response.status)
+              })
+        }
+        }>request to backend</button><br/>
       </header>
     </div>
   );
