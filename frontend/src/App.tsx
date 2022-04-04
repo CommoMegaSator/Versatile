b36c0fd457b7cf8b0ec754937e0af853046007ec
RabbitAlgedo <komarenecn@gmail.com>
2022-04-04 16:31:35 +0300

    add redux | redux toolkit support typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import store from "./store";
+import { Provider } from 'react-redux'
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={function (){
-          axios.get('http://localhost:5000')
-              .then(function (response) {
-                alert(response.status)
-              })
-              .catch(()=>alert("Server is not responding"))
-        }
-        }>request to backend</button><br/>
-      </header>
-    </div>
+    <Provider store={store}>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <button onClick={function (){
+                    axios.get('http://localhost:5000')
+                        .then(function (response) {
+                            alert(response.status)
+                        })
+                        .catch(()=>alert("Server is not responding"))
+                }
+                }>request to backend</button><br/>
+            </header>
+        </div>
+    </Provider>
   );
 }
 
